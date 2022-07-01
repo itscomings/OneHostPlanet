0cd8c1ab3cfb6c9afebaea583d1ce05efe60eafa
Evgeniy Dubinin <evgeniy@OnevoiPlanets-iMac.local>
2022-07-01 16:59:36 +0300

    scrips edit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        stagePadding: 50,
+        loop:true,
+            margin: 20,
+            nav: true,
+            dots: false,
+            autoWidth:true,
+            navElement: "arrow",
+
+        }
+    )
+})
