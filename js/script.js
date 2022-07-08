42ea2b9f4d0ea5078c8608510e9541f25876c88d
Evgeniy Dubinin <evgeniy@OnevoiPlanets-iMac.local>
2022-07-08 15:32:12 +0300

    mobile version

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        stagePadding: 50,
-        loop:true,
+$(document).ready(function () {
+
+
+    $('.stock__cards').owlCarousel({
+            loop: true,
             margin: 20,
             nav: true,
             dots: false,
-            autoWidth:true,
+            autoWidth: true,
             navElement: "arrow",
 
         }
-    )
+    );
+
+    $('.news__cards').owlCarousel({
+            margin: 20,
+            response: false,
+            dots: false,
+            autoWidth: true,
+            navElement: "arrow",
+        }
+    );
+
+    $('.best__cards').owlCarousel({
+
+            margin: 20,
+            response: false,
+            dots: false,
+            autoWidth: true,
+            navElement: "arrow",
+        }
+    );
+
+
 })
+
