c15570f04822fc0c072c7afdd31f44b4c2cdddef
Evgeniy Dubinin <evgeniy@OnevoiPlanets-iMac.local>
2022-07-08 16:26:07 +0300

    mobile version2

--- js/script.js
+++ js/script.js
@@ -1,36 +1,43 @@
 $(document).ready(function () {
 
+    // $('.best__cards').owlCarousel({
+    //         slideTransition: "linear" ,
+    //         margin: 20,
+    //         response: false,
+    //         dots: false,
+    //         autoWidth: true,
+    //         navElement: "arrow",
+    //     }
+    // );
 
     $('.stock__cards').owlCarousel({
+            slideTransition: "linear",
             loop: true,
             margin: 20,
             nav: true,
             dots: false,
             autoWidth: true,
             navElement: "arrow",
-
         }
     );
 
-    $('.news__cards').owlCarousel({
-            margin: 20,
-            response: false,
-            dots: false,
-            autoWidth: true,
-            navElement: "arrow",
-        }
-    );
-
-    $('.best__cards').owlCarousel({
-
-            margin: 20,
-            response: false,
-            dots: false,
-            autoWidth: true,
-            navElement: "arrow",
-        }
-    );
-
+    // $('.news__cards').owlCarousel({
+    //         slideTransition: "linear",
+    //         margin: 20,
+    //         response: false,
+    //         dots: false,
+    //         autoWidth: true,
+    //         navElement: "arrow",
+    //     }
+    // );
 
 })
+    /* Open nav-menu */
+    function openNav() {
+        document.getElementById("myNav").style.height = "100vh";
+    }
+    /* Close nav-menu*/
+    function closeNav() {
+        document.getElementById("myNav").style.height = "0";
+    }
 
